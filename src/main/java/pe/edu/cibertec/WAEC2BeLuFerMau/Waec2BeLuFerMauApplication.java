7735c77f317faa1c6e6e5eecb4f0e8a89eac2d45
Fernando <monasoxx@gmail.com>
2024-04-27 00:24:20 -0500

    prueba3

--- src/main/java/pe/edu/cibertec/WAEC2BeLuFerMau/Waec2BeLuFerMauApplication.java
+++ src/main/java/pe/edu/cibertec/WAEC2BeLuFerMau/Waec2BeLuFerMauApplication.java
@@ -1,5 +1,6 @@
 package pe.edu.cibertec.WAEC2BeLuFerMau;
 
+//Nuevo comentario
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
