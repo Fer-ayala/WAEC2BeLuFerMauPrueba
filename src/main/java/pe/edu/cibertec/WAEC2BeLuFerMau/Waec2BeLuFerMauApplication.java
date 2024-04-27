f963c72cc04af9c180d14c63ce74f51003cdf5b4
Belenn333 <kiaraferh@gmail.com>
2024-04-26 23:24:56 -0500

    prueba 1

---
+++ src/main/java/pe/edu/cibertec/WAEC2BeLuFerMau/Waec2BeLuFerMauApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2BeLuFerMau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2BeLuFerMauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2BeLuFerMauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/WAEC2BeLuFerMau/Waec2BeLuFerMauApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2BeLuFerMau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2BeLuFerMauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
